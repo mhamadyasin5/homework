504e4e80d4ae409ac4333f6086faa4a4462877fd
mhamadyasin5 <mohammed.200052855666@spu.edu.iq>
2022-04-06 22:13:51 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cont">
+        <div class="item">
+            <img src="img/277826283_3139773049637790_1080263274368097155_n.jpg" alt="" width="300" height="300" style="border-radius: 20px">
+            <h1>Mohammed Yasin</h1>
+            <p>my name is mhamad <a href="profile">show more</a></p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cont">
+        <div class="item">
+            <img src="img/277835264_1609884546043221_1970634507541782296_n.jpg" alt="" width="300" height="400" style="border-radius: 20px">
+            <h1>Mohammed Yasin</h1>
+            <p>talabay IT y 3em zor bashm la front end  </p>
+            <a href="{{route("hl")}}">home</a>
+        </div>
+    </div>
+</body>
+</html>
